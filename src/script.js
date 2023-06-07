68809ab26a251fffb13606ce9b9182cee076c154
kiiru-sn <kiiru.sn@gmail.com>
2023-06-07 18:04:25 +0100

    updated netlify link and removed previous javascript

--- src/script.js
+++ src/script.js
@@ -15,22 +15,3 @@ let tenYearsFromNow = moment()
   .format("dddd, MMMM Do, YYYY H:mm:s");
 let tenYearsElement = document.querySelector("#tenYearsFromNow");
 tenYearsElement.innerHTML = `10 years from now, it will be ${tenYearsFromNow}`;
-
-let localTimeZone = moment.tz.guess();
-let localElement = document.querySelector("#local");
-localElement.innerHTML = `Your current Time Zone is ${localTimeZone} and the current time is ${moment().format(
-  "H:m"
-)}`;
-
-let honoluluDate = moment()
-  .tz("Pacific/Honolulu")
-  .format("dddd, MMMM D, YYYY h:m A");
-let honoluluElement = document.querySelector("#honolulu");
-honoluluElement.innerHTML = `It is ${honoluluDate} in Honolulu, Hawaii`;
-
-let parisDate = moment()
-  .tz("Europe/Paris")
-  .add(10, "years")
-  .format("dddd, MMMM D, YYYY h:m A");
-let parisElement = document.querySelector("#parisTen");
-parisElement.innerHTML = `10 years from now, it will be ${parisDate} in Paris, France`;
