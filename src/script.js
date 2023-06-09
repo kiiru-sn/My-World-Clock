6bf53ec863f7851989c86336aa5b32a3143d5e80
kiiru-sn <kiiru.sn@gmail.com>
2023-06-09 13:10:30 +0100

    added link to all cities

--- src/script.js
+++ src/script.js
@@ -54,7 +54,9 @@ function updateCity(event) {
           <div class="time">${cityTime.format(
             "h:mm:ss"
           )}<small> ${cityTime.format("A")}</small></div>
-        </div>`;
+        </div>
+        <center><a href="index.html">All Cities</a></center>
+        `;
 }
 
 updateTime();
